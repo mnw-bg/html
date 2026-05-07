d9c4dc18bedfb3caa75c7152c6b8c05d7cf0c720
warayut <warayut100720@gmail.com>
2026-05-07 07:17:30 +0300

    t

---
+++ ProjeckPizza/ProjeckPizza/Sydan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjeckPizza
+{
+    internal class Sydan
+    {
+		public static class IsoAiti
+		{
+			// tieto maksulle
+			public static double kuiti = 0;
+
+			// tieto historialle
+			public static double myynti = 0;
+			public static int asiakas = 0;
+
+			// lähetä kuitille
+			public static string kuitisisalto = "";
+			
+		}
+	}
+}
